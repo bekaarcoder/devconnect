88f3f06dd2897ffbcb7fe8c09b03d3a1bd92b2cc
Shashank <shashank_smaty@yahoo.co.in>
2018-06-14 07:12:00 +0530

    connected app to mongo and added routes

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Database Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose.connect(db)
+	.then(() => console.log('MongoDB Connected.'))
+	.catch(err => console.log(err));
+
 app.get('/', (req, res) => res.send('Hello World'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/profile', profile);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
