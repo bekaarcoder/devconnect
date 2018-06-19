54b150aa3a4e4b14d8d46c1d7b7c07863a3ba6bc
Shashank <shashank_smaty@yahoo.co.in>
2018-06-20 01:04:29 +0530

    create andupdate profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -30,4 +30,55 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
 		});
 });
 
+// @router 	POST api/profile
+// @desc 		Create or edit user profile
+// @access 	Private
+router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+	const errors = {};
+	const profileFields = {};
+	profileFields.user = req.user.id;
+	req.body.handle && profileFields.handle = req.body.handle;
+	req.body.company && profileFields.company = req.body.company;
+	req.body.website && profileFields.website = req.body.website;
+	req.body.location && profileFields.location = req.body.location;
+	req.body.status && profileFields.status = req.body.status;
+	req.body.bio && profileFields.bio = req.body.bio;
+	req.body.github && profileFields.github = req.body.github;
+	// skills
+	if(typeof req.body.skills !== undefined) {
+		profileFields.skills = req.body.skills.split(',');
+	}
+	// social media links
+	profileFields.social = {};
+	req.body.facebook && profileFields.social.facebook = req.body.facebook;
+	req.body.twitter && profileFields.social.twitter = req.body.twitter;
+	req.body.linkedin && profileFields.social.linkedin = req.body.linkedin;
+	req.body.instagram && profileFields.social.instagram = req.body.instagram;
+
+	Profile.findOne({user: req.user.id})
+		.then((profile) => {
+			if(profile) {
+				// Update Profile
+				Profile.findOneAndUpdate(
+					{user: req.user.id},
+					{$set: profileFields},
+					{new: true}
+				).then((profile) => res.json(profile));
+			} else {
+				// Create Profile
+				// check if handle exist
+				Profile.findOne({handle: profileFields.handle})
+					.then((profile) => {
+						if(profile) {
+							errors.handle = "Handle already exist";
+							return res.status(400).json(errors);
+						}
+						// Save Profile
+						const newProfile = new Profile(profileFields);
+						newProfile.save().then((profile) => res.json(profile));
+					});
+			}
+		});
+});
+
 module.exports = router;
