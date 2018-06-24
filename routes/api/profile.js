10917e2e25a685f6826d8643574a826d137e0f0c
Shashank <shashank_smaty@yahoo.co.in>
2018-06-24 13:15:04 +0530

    delete profile and user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -239,4 +239,16 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', {session: false
 		}).catch(err => res.status(404).json(err));
 });
 
+// @router 	DELETE api/profile
+// @desc 		Delete User and Profile
+// @access 	Private
+router.delete('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+	Profile.findOneAndRemove({user: req.user.id})
+		.then(() => {
+			User.findOneAndRemove({_id: req.user.id})
+				.then(() => res.json({status: "Success"}))
+				.catch((err) => res.status(400).json(err));
+		}).catch((err) => res.status(400).json(err));
+});
+
 module.exports = router;
