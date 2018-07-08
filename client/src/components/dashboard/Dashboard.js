c00aefc2d29cb1cd669f0c7dd834a817b1551f89
Shashank <shashank_smaty@yahoo.co.in>
2018-07-08 23:48:55 +0530

    Dashboard profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -9,12 +9,33 @@ class Dashboard extends Component {
 	}
 
 	render() {
+		const {user} = this.props.auth;
+		console.log(user);
+		const {profile, loading} = this.props.profile;
+		let dashboardContent;
+		if(profile === null || loading) {
+			dashboardContent = <h4>Loading...</h4>
+		} else {
+			dashboardContent = <h1>Your Profile</h1>
+		}
 		return (
 			<div>
 				<h1>Dashboard</h1>
+				{dashboardContent}
 			</div>
 		)
 	}
 };
 
-export default connect(null, {getUserProfile})(Dashboard);
+Dashboard.propTypes = {
+	getUserProfile: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	profile: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+	profile: state.profile,
+	auth: state.auth
+});
+
+export default connect(mapStateToProps, {getUserProfile})(Dashboard);
