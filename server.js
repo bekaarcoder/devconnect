7fa971428dac60f57d14274b50c82800cfac4425
Shashank <s_shashank@outlook.com>
2018-08-07 00:23:14 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = required('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -31,6 +32,15 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/posts', posts);
 app.use('/api/profile', profile);
+
+// Server static assets if in production
+if(process.env.NODE_ENV === 'production') {
+	// set a static folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
