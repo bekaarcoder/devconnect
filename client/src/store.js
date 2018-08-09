072d84c690a4635f24ce33478019f67e1ca9c57b
Shashank <s_shashank@outlook.com>
2018-08-09 20:26:15 +0530

    changes in store to run app without redux ext

--- client/src/store.js
+++ client/src/store.js
@@ -6,10 +6,15 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
 	rootReducer,
 	initialState,
-	compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+	composeEnhancers(
+		applyMiddleware(...middleware)
+	)
+	// compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 );
 
 export default store;
